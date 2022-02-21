0a59d9c9614e59510d18aa99536e22000b87d572
Mun Hong Fong <fongmh1947@gmail.com>
2022-02-21 16:38:34 -0500

    First commit library.

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+import json
+import sqlite3
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
