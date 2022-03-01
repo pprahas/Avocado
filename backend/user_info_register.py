62400d8eab7808cd47067ee9f8c7e37e6665fe2f
Prahas Pattem <ppattem@purdue.edu>
2022-02-28 19:41:05 -0500

    register_input_checking

--- backend/user_info_register.py
+++ backend/user_info_register.py
@@ -1,16 +1,12 @@
-from base64 import encode
 import json
 import sqlalchemy as db
 import datetime
-from hashlib import sha256
-from datetime import datetime
-import random
-
 
 MSG_REQUEST_NO_BODY = {"status": 500, "statusText": "Requests has no body.", "body": {}}
 MSG_REQUEST_INCORRECT_FORMAT = {"status": 500, "statusText": "Requests incorrect format.", "body": {}}
 MSG_SUCCESS = {"status": 200, "statusText": "User created account successfully.", "body": {}}
-MSG_FAIL_TO_CREATE = {"status": 422, "statusText": "Account creation failed.", "body": {}}
+MSG_FAIL_TO_CREATE = {"status": 422, "statusText": "Input is invalid.", "body": {}}
+
 
 def input_checking( func ):
 
@@ -22,28 +18,54 @@ def input_checking( func ):
 
         """decorator for input checking"""
         try:
-            # assert content.get( "firstName" ), "First Name not found"
-            # assert content.get( "lastName" ), "Last Name not found"
-            # assert content.get( "email" ), "Email not found."
-            # assert content.get( "birthday" ), "Birthday not found."
-            # assert content.get( "password" ), "Password not found."
-            pass
+            assert content.get( "firstName" ), "First Name not found"
+            assert content.get( "lastName" ), "Last Name not found"
+            assert content.get( "email" ), "Email not found."
+            assert content.get( "birthday" ), "Birthday not found."
+            assert content.get( "password" ), "Password not found."
+
+            # assert ";" not in content.get( "firstName" ), "Semicolon is present."
+
+            #firstName input validation
+            firstName = content.get( "firstName" )
+
+            if len(firstName) > 30 and len(firstName < 2):
+                raise Exception("The length of the first name is not the correct length.")
+            if (firstName.isalpha() == False):
+                raise Exception("The first name needs to only contain alphabets.")
+            
+            #lastName input validation
+            lastName = content.get( "lastName" )
+            if len(lastName) > 30 and len(lastName < 2):
+                raise Exception("The length of the first name is not the correct length.")
+            if (lastName.isalpha() == False):
+                raise Exception("The first name needs to only contain alphabets.")
+            
+            #email input validation
+            
+            email = content.get( "email" )
+            birthday = content.get( "birthday" )
+            password = content.get( "password" )
+            if len(firstName) > 4:
+                raise Exception("FirstName is longer than 60 characters")
 
         except Exception as e:
             # return data
             return { "status": 422, "statusText": "Account field missing.", "body": str( e ) }
 
         # return function
-        return func( content, context )
+        return func( event, context )
 
     # return
     return inner
+
 
 def db_connection():
     username = "admin"
     password = "avocado123"
     server = "avocado-348.cgooazgc1htx.us-east-1.rds.amazonaws.com"
     database = "avocado1"
+
 
     db_url = "mysql+pymysql://{}:{}@{}/{}".format(username, password, server, database)
     engine = db.create_engine(db_url, echo=False)
@@ -54,57 +76,24 @@ def db_connection():
 @input_checking   
 def lambda_handler(event, context):
     # TODO implement
-    name = event.get('name')
-    user_id = int(event.get('user_id'))
-    payment_type = event.get('payment_type')
-    birthday = event.get('birthday')
-    user_email = event.get('user_email')
-    user_password = event.get('user_password')
-
-    # running the sha256 hashing algorithm
-    h = sha256()
-    h.update(b'user_password')
-    # user_password = h.hexdigest()
-      
-    # print(name)
-    # print(user_id)
-    # print(payment_type)
-    # print(birthday)
-    # print(user_email)
-    # print(user_password)
-
-    birthday = datetime.strptime(birthday, '%Y-%m-%d')
-    birthday = birthday.strftime('%Y-%m-%d %H:%M:%S')
-
-    # version 1----------------------------------------------------------------------------------------------------------
-    # # take note that string and date need '', integer doesn't.
-
-    # sql = """INSERT INTO user_info (name, user_id, payment_type, birthday, user_email, user_password)
-    #                 VALUES ('{}', {}, '{}', '{}', '{}', '{}');
-    #     """.format(name, user_id, payment_type, 
-    #                 birthday, user_email, user_password)
-
-    # # # connect to db
-    # engine = db_connection()
-
-    # connection = engine.connect()
-
-    # connection.execute(sql)
-
-
-    # version 2----------------------------------------------------------------------------------------------------------
-    sql = "INSERT INTO user_info(name, user_id, payment_type, birthday, user_email, user_password) VALUES (%s, %s, %s, %s, %s, %s)" 
-    val = (name, random.randint(1000,9999999), payment_type, birthday, user_email, user_password)
 
     # connect to db
     engine = db_connection()
 
     connection = engine.connect()
 
-    connection.execute(sql,val)
+    # metadata = db.MetaData()
+    # host = db.Table("host", metadata, autoload=True, autoload_with=engine)
 
 
     try:
+        # rows = connection.execute(
+        # """
+        # SELECT * FROM host
+        # """)
+
+        # for r in rows:
+        #     print(r)
 
         return MSG_SUCCESS
 
@@ -116,12 +105,11 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     body = {
-        "name": "Bulldog2123",
-        "user_id": "435345",
-        "payment_type": "Cash13",
-        "birthday": "2001-11-05",
-        "user_email": "bulldog@gmail.commm",
-        "user_password": "pwefwf@2ht3"
+        "firstName": "Jff343",
+        "lastName": "Fong",
+        "email": "dwejio@google.com",
+        "birthday": "02-05-2020",
+        "password": "djweoij"
     }
 
     event = {
