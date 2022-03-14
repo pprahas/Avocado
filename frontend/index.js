6e35c42338882824626f1160ad297f5bf6d9415c
Mun Hong Fong <fongmh1947@gmail.com>
2022-03-14 17:02:58 -0400

    homepage js modification.

--- frontend/index.js
+++ frontend/index.js
@@ -8,5 +8,24 @@ fetch('https://5kwlyceua3.execute-api.us-east-1.amazonaws.com/default/home-most_
     body: JSON.stringify(data)
 }).then((res) => res.json())
 .then((data) => {
-    console.log(data);
+    // console.log(data);
+    let output = "";
+    data.forEach(function(restaurant) {
+        // console.log(restaurant.image);
+        // output += `
+        // <div>
+        // <p>Some</p>
+        //     <img src="data:image/png;base64, ${restaurant.image}" alt="Red dot" />
+        // </div>
+        // `
+
+        output += `
+        <div class="rest_container">
+            <a class="rest_link" href="rest_pandaexpress.html"><img class="rest_image "src="data:image/png;base64, ${restaurant.image}" alt="Red dot" /></a>
+            ${restaurant.rest_name}
+        </div>
+        `
+
+    });
+    document.getElementById('most_popular').innerHTML = output;
 })
