2f222819b0ff64caf5a39ca442dc32298c6dde9c
soohong ahn <soohongahn@pal-10-184-85-46.itap.purdue.edu>
2022-03-24 14:20:54 -0400

    loading pages

--- frontend/cart.js
+++ frontend/cart.js
@@ -1,40 +1,17 @@
 let user_email = localStorage.getItem("user_email");
 
-if (user_email == null){
-  console.log("NOT LOGGED IN!");
-  window.location = "login.html";
-}
-
 const ele = document.getElementById("cart_list");
-const submit_order = document.getElementById("submit_order");
-submit_order.addEventListener('click', submitOrder());
-
-function submitOrder(){
-  const data = {
-      user_email: user_id,
-      rest_id: rest_id,
-      food_id: this.id,
-  };
-
-  fetch('https://6h77675zrl.execute-api.us-east-1.amazonaws.com/default/restaurants-add_to_cart', {
-  method: 'POST',
-  body: JSON.stringify(data)
-  }).then((res) => res.json())
-  .then((data) => {
-      if (data.status === 200) {
-          console.log(data.statusText);
-      } else {
-          console.log(data.statusText);
-      }
-  });
-}
-
 
 let cart_list;
 
 const data = {
     user_email: user_email
 };
+
+if (user_email == null){
+  console.log("NOT LOGGED IN!");
+  window.location = "login.html";
+}
 
 async function displaycart() {
     fetch('https://q6y9jbmbwl.execute-api.us-east-1.amazonaws.com/default/cart-list_of_items', {
