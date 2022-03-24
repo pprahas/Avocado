4c2ef8e8c939a3fd4e3c4185e933cab81809a5f8
soohong ahn <soohongahn@pal-10-184-85-46.itap.purdue.edu>
2022-03-24 13:50:06 -0400

    loading page

--- backend/cart/cart_submit_order.py
+++ backend/cart/cart_submit_order.py
@@ -1,16 +1,39 @@
-import uuid
-import json
 import sqlalchemy as db
-from datetime import date
-import cart_list_of_items as Cart
+import json
 
 MSG_REQUEST_NO_BODY = {"status": 500, "statusText": "Requests has no body.", "body": {}}
 MSG_REQUEST_INCORRECT_FORMAT = {"status": 500, "statusText": "Requests incorrect format.", "body": {}}
-MSG_SUCCESS = {"status": 200, "statusText": "User created account successfully.", "body": {}}
-MSG_FAIL_TO_CREATE = {"status": 422, "statusText": "Account creation failed.", "body": {}}
+MSG_ADD_SUCCESS = {"status": 200, "statusText": "The item has been added into cart successfully.", "body": {}}
+MSG_UPDATE_SUCCESS = {"status": 200, "statusText": "Quantity updated in cart successfully.", "body": {}}
+MSG_ID_NOT_FOUND = {"status": 422, "statusText": "User ID not found.", "body": {}}
+MSG_FAIL_TO_CREATE = {"status": 422, "statusText": "Add to cart failed.", "body": {}}
 
-# # Establish Connection
-# from main import MSG_REQUEST_INCORRECT_FORMAT
+
+def input_checking(func):
+    def inner(event, context):
+        try:
+            content = json.loads(event.get("body"))
+        except:
+            return MSG_REQUEST_INCORRECT_FORMAT
+
+        """decorator for input checking"""
+        try:
+            # assert content.get( "firstName" ), "First Name not found"
+            # assert content.get( "lastName" ), "Last Name not found"
+            # assert content.get( "email" ), "Email not found."
+            # assert content.get( "birthday" ), "Birthday not found."
+            # assert content.get( "password" ), "Password not found."
+            pass
+
+        except Exception as e:
+            # return data
+            return {"status": 422, "statusText": "Restaurant ID field missing.", "body": str(e)}
+
+        # return function
+        return func(content, context)
+
+    # return
+    return inner
 
 
 def db_connection():
@@ -25,67 +48,74 @@ def db_connection():
 
     return engine
 
-# Check for Input
-def input_checking(func):
-    def inner(event, context):
-        try:
-            content = json.loads(event.get("body"))
-        except:
-            return MSG_REQUEST_INCORRECT_FORMAT
 
-        """decorator for input checking"""
-        try:
-            assert content.get( "user_id" ), "User ID not found"
-            assert content.get( "options" ), "Option not found"
-            pass
-
-        except Exception as e:
-            # return data
-            return {"status": 422, "statusText": "Account field missing.", "body": str(e)}
-
-        # return function
-        return func(content, context)
-
-    # return
-    return inner
-
-# Obtain rows from Order History Table
 @input_checking
 def lambda_handler(event, context):
+    # TODO implement
+    user_email = event.get('user_email')
+    rest_id = int(event.get('rest_id'))
+    food_id = int(event.get('food_id'))
 
-    # Connect to DB
+    # connect to db
     engine = db_connection()
+
     connection = engine.connect()
+    # getting user_id from user_info and rest_name from rest_info
+    sql = "SELECT user_id FROM avocado1.user_info WHERE user_email = %s;"
+    val = user_email
+    user_id = connection.execute(sql, val).fetchone()
 
-    user_id = int(event.get('user_id'))
-    option = int(event.get('options'))
+    if not user_id:
+        return MSG_ID_NOT_FOUND
 
-    today = date.today()
-    status = 1
-    order_number = uuid.uuid4().int % 2147483647
+    user_id = user_id.user_id
 
-    cart = Cart.lambda_handler(event, context)
+    rest_sql = "SELECT * FROM rest_info WHERE rest_id = %s;"
+    val = (rest_id)
+    restaurant = connection.execute(rest_sql, val).fetchone()
 
-    print(cart['body'])
+    menu_sql = "SELECT * FROM menu_info WHERE rest_id = %s AND food_id = %s;"
+    val = (rest_id, food_id)
+    food = connection.execute(menu_sql, val).fetchone()
 
-    # sql = "INSERT INTO order_history(order_number, user_id, rest_id, price, options, status, order_date) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    # value = (order_number, user_id, rest_id, price, option, status, today)
+    # saving cart row
+    # check if menu already exist in cart
+    cart_checking_sql = "SELECT * FROM cart WHERE user_id = '" + str(user_id) + "' AND food_id = " + str(food_id) + ";"
+    # val3 = (user_id, food_id)
 
-    # connection.execute(sql, value)
-    print("\nSuccessfully submited order\n")
+    order = connection.execute(cart_checking_sql).fetchall()
 
+    # 이 트라이는 항상 있어야 함
     try:
-        return MSG_SUCCESS
+        if not (order):
+            print('okay')
+            sql4 = "INSERT INTO cart(user_id,food_id,quantity, price, food_name, rest_id, rest_name) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+            # val = (user_id, food_id, 1, food.price, food.name, rest_id, )
+            val = (user_id, food_id, 1, food.price, food.food_name, rest_id, restaurant.name)
+            connection.execute(sql4, val)
+
+            return MSG_ADD_SUCCESS
+
+        else:
+            sql5 = "SELECT quantity FROM avocado1.cart WHERE food_id = " + str(food_id)
+            numFood = connection.execute(sql5).fetchone()
+            numFood = numFood[0]
+
+            sql6 = "UPDATE cart SET quantity = " + str(numFood + 1) + " WHERE food_id = " + str(food_id)
+            connection.execute(sql6)
+            return MSG_UPDATE_SUCCESS
+
     except Exception as e:
-        print(e)
         return MSG_FAIL_TO_CREATE
 
 
 if __name__ == "__main__":
     body = {
-        "user_id": "5000",
-        "options": "1"
+        "user_email": "sum@c8o.uk",
+        "rest_id": "1000",
+        "food_id": "1003"
     }
+
     event = {
         "body": json.dumps(body)
     }
@@ -93,3 +123,5 @@ if __name__ == "__main__":
 
     response = lambda_handler(event, context)
     print(response)
+
+
