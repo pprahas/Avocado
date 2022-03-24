dedebc53c7740e15a02d2662be2127cd94ce1cee
Mun Hong Fong <fongmh1947@gmail.com>
2022-03-24 19:26:23 -0400

    submit order lmao.

--- backend/cart/cart_submit_order.py
+++ backend/cart/cart_submit_order.py
@@ -1,3 +1,5 @@
+import uuid
+import json
 import sqlalchemy as db
 from datetime import date
 
@@ -8,6 +10,19 @@ MSG_FAIL_TO_CREATE = {"status": 422, "statusText": "Submit order failed.", "body
 MSG_USER_NOT_EXIST = {"status": 422, "statusText": "User does not exist.", "body": {}}
 
 
+def db_connection():
+    username = "admin"
+    password = "avocado123"
+    server = "avocado-348.cgooazgc1htx.us-east-1.rds.amazonaws.com"
+    database = "avocado1"
+
+    db_url = "mysql+pymysql://{}:{}@{}/{}".format(username, password, server, database)
+    engine = db.create_engine(db_url, echo=False)
+    engine.connect()
+
+    return engine
+
+# Check for Input
 def input_checking(func):
     def inner(event, context):
         try:
@@ -23,7 +38,7 @@ def input_checking(func):
 
         except Exception as e:
             # return data
-            return {"status": 422, "statusText": "Restaurant ID field missing.", "body": str(e)}
+            return {"status": 422, "statusText": "Account field missing.", "body": str(e)}
 
         # return function
         return func(content, context)
@@ -31,35 +46,13 @@ def input_checking(func):
     # return
     return inner
 
-
-def db_connection():
-    username = "admin"
-    password = "avocado123"
-    server = "avocado-348.cgooazgc1htx.us-east-1.rds.amazonaws.com"
-    database = "avocado1"
-
-    db_url = "mysql+pymysql://{}:{}@{}/{}".format(username, password, server, database)
-    engine = db.create_engine(db_url, echo=False)
-    engine.connect()
-
-    return engine
-
-
+# Obtain rows from Order History Table
 @input_checking
 def lambda_handler(event, context):
-    # TODO implement
-    user_email = event.get('user_email')
-    rest_id = int(event.get('rest_id'))
-    food_id = int(event.get('food_id'))
 
-    # connect to db
+    # Connect to DB
     engine = db_connection()
-
     connection = engine.connect()
-    # getting user_id from user_info and rest_name from rest_info
-    sql = "SELECT user_id FROM avocado1.user_info WHERE user_email = %s;"
-    val = user_email
-    user_id = connection.execute(sql, val).fetchone()
 
     user_email = event.get('user_email')
     option = int(event.get('options'))
@@ -106,25 +99,9 @@ def lambda_handler(event, context):
         connection.execute(sql, value)
     
     try:
-        if not (order):
-            print('okay')
-            sql4 = "INSERT INTO cart(user_id,food_id,quantity, price, food_name, rest_id, rest_name) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-            # val = (user_id, food_id, 1, food.price, food.name, rest_id, )
-            val = (user_id, food_id, 1, food.price, food.food_name, rest_id, restaurant.name)
-            connection.execute(sql4, val)
-
-            return MSG_ADD_SUCCESS
-
-        else:
-            sql5 = "SELECT quantity FROM avocado1.cart WHERE food_id = " + str(food_id)
-            numFood = connection.execute(sql5).fetchone()
-            numFood = numFood[0]
-
-            sql6 = "UPDATE cart SET quantity = " + str(numFood + 1) + " WHERE food_id = " + str(food_id)
-            connection.execute(sql6)
-            return MSG_UPDATE_SUCCESS
-
+        return MSG_SUCCESS
     except Exception as e:
+        print(e)
         return MSG_FAIL_TO_CREATE
 
 
@@ -133,7 +110,6 @@ if __name__ == "__main__":
         "user_email": "munhong@gmail.com",
         "options": "1"
     }
-
     event = {
         "body": json.dumps(body)
     }
@@ -141,5 +117,3 @@ if __name__ == "__main__":
 
     response = lambda_handler(event, context)
     print(response)
-
-
