f22b87e6aa779602ea6468238307d0673f3d674a
Prahas Pattem <ppattem@purdue.edu>
2022-04-16 01:43:26 -0400

    .\user-forget_password.py deployed

--- backend/user/user-forget_password.py
+++ backend/user/user-forget_password.py
@@ -7,14 +7,19 @@ import uuid
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-MSG_REQUEST_NO_BODY = {"status": 500, "statusText": "Requests has no body.", "body": {}}
-MSG_REQUEST_INCORRECT_FORMAT = {"status": 500, "statusText": "Requests incorrect format.", "body": {}}
-MSG_SUCCESS = {"status": 200, "statusText": "The unique code has been sent to the user.", "body": {}}
-MSG_FAIL_TO_CREATE = {"status": 422, "statusText": "The user's email address does not exist.", "body": {}}
+MSG_REQUEST_NO_BODY = {"status": 500,
+                       "statusText": "Requests has no body.", "body": {}}
+MSG_REQUEST_INCORRECT_FORMAT = {
+    "status": 500, "statusText": "Requests incorrect format.", "body": {}}
+MSG_SUCCESS = {"status": 200,
+               "statusText": "A unique code has been sent to your email address.", "body": {}}
+MSG_FAIL_TO_CREATE = {
+    "status": 422, "statusText": "The email address you entered does not exist.", "body": {}}
 
-def input_checking( func ):
 
-    def inner( event, context ):
+def input_checking(func):
+
+    def inner(event, context):
         try:
             content = json.loads(event.get("body"))
         except:
@@ -24,20 +29,21 @@ def input_checking( func ):
         try:
             # assert content.get( "firstName" ), "First Name not found"
             # assert content.get( "lastName" ), "Last Name not found"
-            assert content.get( "user_email" ), "Email address not found."
+            assert content.get("user_email"), "Email address not found."
             # assert content.get( "birthday" ), "Birthday not found."
             # assert content.get( "password" ), "Password not found."
             pass
 
         except Exception as e:
             # return data
-            return { "status": 422, "statusText": "Account field missing.", "body": str( e ) }
+            return {"status": 422, "statusText": "Account field missing.", "body": str(e)}
 
         # return function
-        return func( content, context )
+        return func(content, context)
 
     # return
     return inner
+
 
 def db_connection():
     username = "admin"
@@ -45,45 +51,48 @@ def db_connection():
     server = "avocado-348.cgooazgc1htx.us-east-1.rds.amazonaws.com"
     database = "avocado1"
 
-    db_url = "mysql+pymysql://{}:{}@{}/{}".format(username, password, server, database)
+    db_url = "mysql+pymysql://{}:{}@{}/{}".format(
+        username, password, server, database)
     engine = db.create_engine(db_url, echo=False)
     engine.connect()
 
     return engine
 
-@input_checking   
+
+@input_checking
 def lambda_handler(event, context):
-   
-    #retrieving from the json file 
+
+    # retrieving from the json file
     user_email = event.get('user_email')
 
     # connect to db
     engine = db_connection()
     connection = engine.connect()
 
-    #checking if the email exists
+    # checking if the email exists
 
-    user_email_query_count = "SELECT COUNT(user_id) FROM avocado1.user_info where user_email = \'" + user_email + "\';"
+    user_email_query_count = "SELECT COUNT(user_id) FROM avocado1.user_info where user_email = \'" + \
+        user_email + "\';"
     # print (user_id_query_count)
-        
+
     user_email_db_count = connection.execute(user_email_query_count)
 
     # print(user_id_db_count)
 
     for row in user_email_db_count:
         user_email_count = row[0]
-    
+
     # print(user_id_count)
 
     if(user_email_count == 0):
-           return MSG_FAIL_TO_CREATE
-    
-    # generate unique id
-    unique_id = uuid.uuid4().hex
+        return MSG_FAIL_TO_CREATE
+
+    # generate unique code
+    unique_code = uuid.uuid4().hex
 
     # sender email address
     email_user = 'cs348avocado@gmail.com'
- 
+
     # sender email password for login purposes
     email_password = 'Cs348-avocado'
 
@@ -103,8 +112,7 @@ def lambda_handler(event, context):
     Good news, you can reset it by entering the unique code that you see below:
 
     """
-
-    body = body + unique_id
+    body = body + unique_code
 
     body = body + """
 
@@ -112,17 +120,23 @@ def lambda_handler(event, context):
 
     Team Avocado Out
     """
-    msg.attach(MIMEText(body,'plain'))
+    msg.attach(MIMEText(body, 'plain'))
     text = msg.as_string()
-    server = smtplib.SMTP('smtp.gmail.com',587)
-   
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+
     try:
         server.starttls()
-        server.login(email_user,email_password)
+        server.login(email_user, email_password)
 
-        server.sendmail(email_user,email_send,text)
+        server.sendmail(email_user, email_send, text)
 
         server.quit()
+
+        sql = "INSERT INTO unique_code_table(user_email, unique_code) VALUES (%s, %s)"
+
+        val = (user_email, unique_code)
+        connection.execute(sql, val)
+
         return MSG_SUCCESS
 
     except Exception as e:
@@ -132,7 +146,7 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     body = {
-        "user_email": "p.prahas@gmaifl.com"
+        "user_email": "p.prahas@gmail.com"
     }
 
     event = {
@@ -142,11 +156,3 @@ if __name__ == "__main__":
 
     response = lambda_handler(event, context)
     print(response)
-
-"""
-Completed: Currently this code checks if the user_email exists and then sends an email with a
-           unique code
-Next Step: Instead of creating a new table to store the unique codes for a particular user. Generating
-           a temp password and sending that over to the email sounds better to me
-"""
-             
